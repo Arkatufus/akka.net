84ff10f5b086ac45b918f5584da44bcfb0510e42
Gregorius Soedharmo <arkatufus@yahoo.com>
2020-07-10 00:05:29 +0700

    implement Typed ClusterSharding

---
+++ src/contrib/cluster/Akka.Cluster.Sharding/Typed/Internal/ClusterShardingImpl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akka.Annotations;
+
+namespace Akka.Cluster.Sharding.Typed.Internal
+{
+
+    [InternalApi]
+    internal class ExtractorAdapter<E, M> : ShardingMessageExtractor<object, M>
+    {
+        public ExtractorAdapter(int numberOfShards) : base(numberOfShards)
+        {
+        }
+
+        public override string EntityId(object message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ShardId(string entityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override M UnwrapMessage(object message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class ClusterShardingImpl
+    {
+    }
+}
