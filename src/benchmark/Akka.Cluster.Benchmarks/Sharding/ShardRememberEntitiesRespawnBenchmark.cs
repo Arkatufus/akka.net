82e4aa28c7e38b813e672f065d760dfd499320f0
Gregorius Soedharmo <arkatufus@yahoo.com>
2023-10-27 13:14:15 +0700

    Update code

--- src/benchmark/Akka.Cluster.Benchmarks/Sharding/ShardRememberEntitiesRespawnBenchmark.cs
+++ src/benchmark/Akka.Cluster.Benchmarks/Sharding/ShardRememberEntitiesRespawnBenchmark.cs
@@ -19,7 +19,7 @@ using static Akka.Cluster.Benchmarks.Sharding.ShardingHelper;
 namespace Akka.Cluster.Benchmarks.Sharding
 {
     [Config(typeof(MonitoringConfig))]
-    [SimpleJob(RunStrategy.ColdStart, targetCount:1, warmupCount:0, launchCount:5)]
+    [SimpleJob(RunStrategy.ColdStart, warmupCount:0, launchCount:5)]
     public class ShardRememberEntitiesRespawnBenchmark
     {
         [Params(StateStoreMode.Persistence, StateStoreMode.DData)]
